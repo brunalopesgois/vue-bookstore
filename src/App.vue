96209df59b22b11ae866f374d075126c915767e6
Bruna Lopes <bruna-chan@live.com>
2021-07-11 20:25:05 -0300

    use bootstrap vue and main page design

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,71 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+      <header>
+        <b-jumbotron bg-variant="warning" text-variant="black">
+          <template #header>Livraria ABC</template>
+
+        <nav>
+          <b-navbar class="d-flex justify-content-between" toggleable="lg" type="dark" variant="dark">
+            <b-navbar-brand href="#">Home</b-navbar-brand>
+            <b-navbar-nav class="ml-auto">
+              <b-nav-item href="#">Log In</b-nav-item>
+            </b-navbar-nav>
+          </b-navbar>
+        </nav>
+        </b-jumbotron>
+      </header>
+
+      <section>
+        <ul class="book-list">
+          <li>
+            <div class="panel">
+              <img class="img" src="https://img.r7.com/images/dia-internacional-do-gato-instagram-08082019161529430?dimensions=771x420&no_crop=true" alt="">
+              <h3>R$ 23.56</h3>
+              <p>Gato astrounauta</p>
+              <b-button variant="warning">Comprar</b-button>
+            </div>
+          </li>
+        </ul>
+      </section>
+
+      <footer></footer>
   </div>
 </template>
 
 <script>
+import { BContainer } from 'bootstrap-vue';
+import { BJumbotron } from 'bootstrap-vue';
+import { BNavbar } from 'bootstrap-vue';
+import { BNavbarNav } from 'bootstrap-vue';
+import { BNavbarBrand } from 'bootstrap-vue';
+import { BButton } from 'bootstrap-vue';
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+  components: {
+    'b-container': BContainer,
+    'b-jumbotron': BJumbotron,
+    'b-navbar': BNavbar,
+    'b-navbar-nav': BNavbarNav,
+    'b-navbar-brand': BNavbarBrand,
+    'b-button': BButton
   }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style>
+.book-list {
+  list-style: none;
 }
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
+.panel {
+  padding: 10px;
+  border: solid 2px black;
   display: inline-block;
-  margin: 0 10px;
+  margin: 5px;
+  width: 240px;
+  height: 100%;
+  vertical-align: top;
+  text-align: center;
 }
-
-a {
-  color: #42b983;
+.img {
+  width: 100%;
 }
 </style>
