12a37b764d4824612ff0ac0005fabd2eca1cf79f
Bruna Lopes <bruna-chan@live.com>
2021-07-16 20:15:34 -0300

    panel component

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -2,27 +2,7 @@
   <b-container fluid>
     <ul class="book-list">
       <li class="book-list-item" v-for="book of books" :key="book.id">
-        <div class="panel">
-          <div class="panel-admin">
-            <b-button-group size="sm">
-              <b-button variant="info">
-                <img src="/src/assets/edit-icon.png">
-              </b-button>
-              <b-button variant="danger">
-                <img src="/src/assets/exit-icon.png">
-              </b-button>
-            </b-button-group>
-          </div>
-          <div class="panel-content">
-            <img class="img" v-if="book.cover" :src="'http://localhost:8000/'+book.cover">
-            <img class="img" v-else src="/src/assets/no-image.png">
-            <h3>R$ {{ book.sale_price }}</h3>
-            <p>{{ book.title }}</p>
-            <div class="btn-alignment">
-              <b-button variant="warning">Comprar</b-button>
-            </div>
-          </div>
-        </div>
+        <my-panel :content="book"></my-panel>
       </li>
     </ul>
   </b-container>
@@ -30,8 +10,7 @@
 
 <script>
 import { BContainer } from 'bootstrap-vue';
-import { BButton } from 'bootstrap-vue';
-import { BButtonGroup } from 'bootstrap-vue';
+import Panel from '../shared/panel/Panel.vue';
 export default {
   data() {
     return {
@@ -46,8 +25,7 @@ export default {
   },
   components: {
     'b-container': BContainer,
-    'b-button': BButton,
-    'b-button-group': BButtonGroup
+    'my-panel': Panel
   }
 }
 </script>
@@ -58,32 +36,5 @@ export default {
 }
 .book-list .book-list-item {
   display: inline-block;
-}
-.panel {
-  position: relative;
-  padding: 10px;
-  border: solid 2px black;
-  display: inline-block;
-  margin: 15px;
-  width: 260px;
-  height: 100%;
-  min-height: 500px;
-  max-height: 500px;
-  vertical-align: top;
-}
-.panel-admin {
-  text-align: end;
-}
-.panel-content {
-  text-align: center;
-}
-.img {
-  width: 100%;
-  height: 300px;
-}
-.btn-alignment {
-  position: absolute;
-  bottom: 15px;
-  left: 83px;
 }
 </style>
---
+++ src/components/shared/panel/Panel.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="panel">
+    <div class="panel-admin-buttons">
+      <b-button-group size="sm">
+        <b-button variant="info">
+          <img src="/src/assets/edit-icon.png">
+        </b-button>
+        <b-button variant="danger">
+          <img src="/src/assets/exit-icon.png">
+        </b-button>
+      </b-button-group>
+    </div>
+    <div class="panel-content">
+      <img class="img" v-if="content.cover" :src="'http://localhost:8000/'+content.cover">
+      <img class="img" v-else src="/src/assets/no-image.png">
+      <h3>R$ {{ content.sale_price }}</h3>
+      <p>{{ content.title }}</p>
+      <div class="btn-alignment">
+        <b-button variant="warning">Saiba Mais</b-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { BButton } from 'bootstrap-vue';
+import { BButtonGroup } from 'bootstrap-vue';
+export default {
+  props: ['content'],
+  components: {
+    'b-button': BButton,
+    'b-button-group': BButtonGroup
+  }
+}
+</script>
+
+<style>
+.panel {
+  position: relative;
+  padding: 10px;
+  border: solid 2px black;
+  display: inline-block;
+  margin: 15px;
+  width: 260px;
+  height: 100%;
+  min-height: 500px;
+  max-height: 500px;
+  vertical-align: top;
+}
+.panel-admin-buttons {
+  text-align: end;
+}
+.panel-content {
+  text-align: center;
+}
+.img {
+  width: 100%;
+  height: 300px;
+}
+.btn-alignment {
+  position: absolute;
+  bottom: 15px;
+  left: 83px;
+}
+</style>
