1da4299d284e0da43d76a13958007f1552b07a43
Bruna Lopes <bruna-chan@live.com>
2021-07-16 19:46:09 -0300

    navbar component

--- src/components/shared/header/Header.vue
+++ src/components/shared/header/Header.vue
@@ -3,33 +3,20 @@
     <b-jumbotron class="d-flex justify-content-center" bg-variant="warning" text-variant="black">
       <template #header>Livraria ABC</template>
     </b-jumbotron>
-    <nav>
-      <b-navbar class="navbar d-flex justify-content-between" toggleable="lg" type="dark" variant="dark">
-        <b-navbar-brand href="#">Home</b-navbar-brand>
-        <b-navbar-brand href="#">Entrar</b-navbar-brand>
-      </b-navbar>
-    </nav>
+    <my-navbar></my-navbar>
   </header>
 </template>
 
 <script>
 import { BJumbotron } from 'bootstrap-vue';
-import { BNavbar } from 'bootstrap-vue';
-import { BNavbarNav } from 'bootstrap-vue';
-import { BNavbarBrand } from 'bootstrap-vue';
+import Navbar from '../navbar/Navbar.vue';
 export default {
   components: {
     'b-jumbotron': BJumbotron,
-    'b-navbar': BNavbar,
-    'b-navbar-nav': BNavbarNav,
-    'b-navbar-brand': BNavbarBrand
+    'my-navbar': Navbar
   }
 }
 </script>
 
 <style>
-.navbar {
-  width: 100%;
-  padding-left: 15px;
-}
 </style>
---
+++ src/components/shared/navbar/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav>
+    <b-navbar class="navbar d-flex justify-content-between" toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand href="#">Home</b-navbar-brand>
+      <b-navbar-brand href="#">Entrar</b-navbar-brand>
+    </b-navbar>
+  </nav>
+</template>
+
+<script>
+import { BNavbar } from 'bootstrap-vue';
+import { BNavbarNav } from 'bootstrap-vue';
+import { BNavbarBrand } from 'bootstrap-vue';
+export default {
+  components: {
+    'b-navbar': BNavbar,
+    'b-navbar-nav': BNavbarNav,
+    'b-navbar-brand': BNavbarBrand
+  }
+}
+</script>
+
+<style>
+.navbar {
+  width: 100%;
+  padding-left: 15px;
+}
+</style>
