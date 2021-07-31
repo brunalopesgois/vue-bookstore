a633a05d9a4cf9294665c27c548c9dde3ea346bd
Bruna Lopes <bruna-chan@live.com>
2021-07-31 15:34:10 -0300

    excludes unused style

--- src/components/register/Register.vue
+++ src/components/register/Register.vue
@@ -204,14 +204,6 @@ export default {
 </script>
 
 <style>
-.register-section {
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
-  width: 50%;
-  padding: 50px;
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-}
 .form-btn {
   margin-top: 20px;
 }
