845d8e1de24cbcdc1601e9d606b1471b9da63c13
Bruna Lopes <bruna-chan@live.com>
2021-08-14 16:04:58 -0300

    implements logout

--- src/components/shared/navbar/Navbar.vue
+++ src/components/shared/navbar/Navbar.vue
@@ -1,9 +1,18 @@
 <template>
-  <nav>
-    <b-navbar class="navbar d-flex justify-content-between" toggleable="lg" type="dark" variant="dark">
-      <b-navbar-brand :to="{ name: 'home' }">Home</b-navbar-brand>
-      <b-navbar-brand :to="{ name: 'login' }">Entrar</b-navbar-brand>
-    </b-navbar>
+  <nav class="navigation">
+    <b-row>
+      <b-navbar toggleable="lg" type="dark" variant="dark">
+        <b-col>
+          <b-navbar-brand id="nav-item" :to="{ name: 'home' }">Home</b-navbar-brand>
+        </b-col>
+        <b-col class="d-flex justify-content-end">
+          <b-navbar-nav>
+            <b-nav-item id="nav-item" :to="{ name: 'login' }">Entrar</b-nav-item>
+            <b-nav-item id="nav-item" @click.prevent="logout()" :to="{ name: 'login' }">Sair</b-nav-item>
+          </b-navbar-nav>
+        </b-col>
+      </b-navbar>
+    </b-row>
   </nav>
 </template>
 
@@ -11,18 +20,33 @@
 import { BNavbar } from 'bootstrap-vue';
 import { BNavbarNav } from 'bootstrap-vue';
 import { BNavbarBrand } from 'bootstrap-vue';
+import { BNavItem } from 'bootstrap-vue';
+import { BRow } from 'bootstrap-vue';
+import { BCol } from 'bootstrap-vue';
 export default {
   components: {
     'b-navbar': BNavbar,
     'b-navbar-nav': BNavbarNav,
-    'b-navbar-brand': BNavbarBrand
+    'b-navbar-brand': BNavbarBrand,
+    'b-row': BRow,
+    'b-col': BCol,
+    'b-nav-item': BNavItem
+  },
+  methods: {
+    logout() {
+      localStorage.clear();
+    }
   }
 }
 </script>
 
 <style>
-.navbar {
-  width: 100%;
+.navigation {
+  max-width: 100%;
+  overflow-x: hidden;
+}
+#nav-item {
   padding-left: 15px;
+  padding-right: 15px;
 }
 </style>
