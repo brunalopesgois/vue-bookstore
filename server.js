753287855e72a642a5b6ea5d07d03fc3f8d71a23
Bruna Lopes <bruna-chan@live.com>
2021-09-07 14:55:50 -0300

    history mode

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var path = require('path')
 var serveStatic = require('serve-static')
 
 var app = express()
+app.use(history())
 app.use(serveStatic(path.join(__dirname, 'dist')))
 
 var port = process.env.PORT || 5000
