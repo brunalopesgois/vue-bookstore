4c2e924b0ec2d782a7ce067ddafa4f423287d85e
Bruna Lopes <bruna-chan@live.com>
2021-09-07 17:24:28 -0300

    back to not using proxy

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
 var history = require('connect-history-api-fallback');
 
 var express = require('express')
-var cors = require('cors')
 var path = require('path')
 var serveStatic = require('serve-static')
 app = express()
 //add this middleware
 app.use(history());
 app.use(serveStatic(__dirname))
-var port = 80
+var port = process.env.PORT || 5000
 app.listen(port)
 console.log('server started '+ port)
-var apiProxy = createProxyMiddleware('/api', { target: 'https://laravel-bookstore-api.herokuapp.com/' });
-var frontendProxy = createProxyMiddleware('/', { target: 'https://livrariabc.herokuapp.com/' });
-
-app.use(apiProxy);
-app.use(frontendProxy);
